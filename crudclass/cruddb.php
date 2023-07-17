cf2e817ea884048b0f44d984ca119084ab25a8a3
Rajveer <110935845+rajveer1122@users.noreply.github.com>
2023-07-17 18:00:04 +0530

    Update cruddb.php

--- crudclass/cruddb.php
+++ crudclass/cruddb.php
@@ -5,6 +5,7 @@ define('DB_SERVER', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_NAME', 'crud');
+//DB- crud, TABLE- student, COL- name,email,course,phone
 
 class crud_con
 {
